f8d846173e1e215ce3050e0cbe518cbb5875f78e
Mariane Carvalho <mariane.carvalho@ccc.ufcg.edu.br>
2024-03-01 10:45:47 -0300

    fix assert import

--- gerenciador_de_tarefas/test/GerenciadorTest.java
+++ gerenciador_de_tarefas/test/GerenciadorTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class GerenciadorTest {
 
     private Gerenciador gerenciador;
