499a6a7888d2736acda0008dbd1a249e4e851822
Mariane Carvalho <mariane.carvalho@ccc.ufcg.edu.br>
2024-03-01 01:35:35 -0300

    [red bar] adiciona construtor de tarefa

--- gerenciador_de_tarefas/src/Tarefa.java
+++ gerenciador_de_tarefas/src/Tarefa.java
@@ -1,2 +1,14 @@
 public class Tarefa {
+
+    private String titulo;
+    private String descricao;
+    private String data_vencimento;
+    private Prioridade prioridade;
+
+    public Tarefa(String titulo, String descricao, String data_vencimento, Prioridade prioridade) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.data_vencimento = data_vencimento;
+        this.prioridade = prioridade;
+    }
 }
