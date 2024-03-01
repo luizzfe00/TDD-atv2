65ea1cb613f402c03d39a444e4bb39119e447b11
Mariane Carvalho <mariane.carvalho@ccc.ufcg.edu.br>
2024-03-01 10:44:06 -0300

    [fix][red bar] atualiza GerenciadorTest para testar corretamente a criacao de uma tarefa

---
+++ gerenciador_de_tarefas/test/GerenciadorTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GerenciadorTest {
+
+    private Gerenciador gerenciador;
+
+    @BeforeEach
+    public void setup() {
+        this.gerenciador = new Gerenciador();
+
+    }
+
+    @Test
+    public void testCriaTarefa() {
+        assertTrue(this.gerenciador.criaTarefa("Contas", "Pagar as contas segunda", "04/03/2024", Prioridade.ALTA));
+    }
+}
