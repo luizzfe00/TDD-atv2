471d0407537b79ac7790bf35725570708779e00b
Mariane Carvalho <mariane.carvalho@ccc.ufcg.edu.br>
2024-03-01 11:57:33 -0300

    [green bar] adiciona atualizaTarefa em Gerenciador de acordo com o teste

--- gerenciador_de_tarefas/src/Gerenciador.java
+++ gerenciador_de_tarefas/src/Gerenciador.java
@@ -1,10 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Gerenciador {
 
+    private HashMap<String, Tarefa> tarefas;
+
     public Gerenciador() {
+        this.tarefas = new HashMap();
     }
 
     public boolean criaTarefa(String titulo, String descricao, String dataVencimento, Prioridade prioridade){
         Tarefa tarefa = new Tarefa(titulo, descricao, dataVencimento, prioridade);
+        this.tarefas.put(titulo, tarefa);
+        return true;
+    }
+
+    public boolean atualizaTarefa (String titulo, String campo, String novoValor) {
+        Tarefa tarefa = this.tarefas.get(titulo);
+
+        switch (campo){
+            case "1":
+                tarefa.setTitulo(novoValor);
+                this.tarefas.remove(titulo);
+                this.tarefas.put(tarefa.getTitulo(), tarefa);
+            case "2":
+                tarefa.setDescricao(novoValor);
+            case "3":
+                tarefa.setDataVencimento(novoValor);
+            case "4":
+                novoValor = novoValor.toLowerCase();
+                if (novoValor.equals("alta")){
+                    tarefa.setPrioridade(Prioridade.ALTA);
+                } else if (novoValor.equals("media")){
+                    tarefa.setPrioridade(Prioridade.MEDIA);
+                } else {
+                    tarefa.setPrioridade(Prioridade.BAIXA);
+                }
+        }
+
+
         return true;
     }
 }
--- gerenciador_de_tarefas/test/GerenciadorTest.java
+++ gerenciador_de_tarefas/test/GerenciadorTest.java
@@ -11,7 +11,8 @@ class GerenciadorTest {
     @BeforeEach
     public void setup() {
         this.gerenciador = new Gerenciador();
-
+        this.gerenciador.criaTarefa("Atividade de vev", "Preciso fazer a atividade de vev até hoje a noite", "01/03/2024", Prioridade.ALTA);
+        this.gerenciador.criaTarefa("Academia", "Ir a academia as 19:00 horas", "02/03/2024", Prioridade.BAIXA);
     }
 
     @Test
@@ -21,8 +22,15 @@ class GerenciadorTest {
     }
 
     @Test
-    public void testAtualizaTarefa(String titulo, String campo, String novoValor) {
-        boolean status = this.gerenciador.atualizaTarefa(titulo, campo, novoValor);
-        assertTrue(status);
+    public void testAtualizaTarefa() {
+        boolean status1 = this.gerenciador.atualizaTarefa("Academia", "1", "Yoga");
+        boolean status2 = this.gerenciador.atualizaTarefa("Yoga", "2", "ir segunda");
+        boolean status3 = this.gerenciador.atualizaTarefa("Yoga", "3", "04/03/2024");
+        boolean status4 = this.gerenciador.atualizaTarefa("Yoga", "4", "alta");
+
+        assertTrue(status1);
+        assertTrue(status2);
+        assertTrue(status3);
+        assertTrue(status4);
     }
 }
