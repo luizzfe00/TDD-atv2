c2aa3cbd45c89cfbd74cc28e746f12d9dc80fe4e
Mariane Carvalho <mariane.carvalho@ccc.ufcg.edu.br>
2024-03-01 10:58:44 -0300

    [green bar] adiciona criaTarefa em Gerenciador

--- gerenciador_de_tarefas/src/Gerenciador.java
+++ gerenciador_de_tarefas/src/Gerenciador.java
@@ -1,2 +1,10 @@
 public class Gerenciador {
+
+    public Gerenciador() {
+    }
+
+    public boolean criaTarefa(String titulo, String descricao, String dataVencimento, Prioridade prioridade){
+        Tarefa tarefa = new Tarefa(titulo, descricao, dataVencimento, prioridade);
+        return true;
+    }
 }
