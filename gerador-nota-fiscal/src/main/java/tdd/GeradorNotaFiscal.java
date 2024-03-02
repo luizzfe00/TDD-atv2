e563c8e406b97183d92d1c05cfaddcb5fe94117a
luizzfe00 <luiz.felipe.farias@ccc.ufcg.edu.br>
2024-03-02 09:03:10 -0300

    criando metodos para gerar nota fiscal

--- gerador-nota-fiscal/src/main/java/tdd/GeradorNotaFiscal.java
+++ gerador-nota-fiscal/src/main/java/tdd/GeradorNotaFiscal.java
@@ -1,4 +1,21 @@
 package tdd;
 
 public class GeradorNotaFiscal {
+
+    private double calculaValorImposto(Fatura fatura) {
+        Double porcentagemImposto = 0.06;
+        switch (fatura.getServico()) {
+            case "CONSULTORIA":
+                porcentagemImposto = 0.25;
+                break;
+            case "TREINAMENTO":
+                porcentagemImposto = 0.15;
+                break;
+        }
+        return fatura.getValor() * porcentagemImposto;
+    }
+    public NotaFiscal gerarNotaFiscal(Fatura fatura) {
+        Double valorImposto = this.calculaValorImposto(fatura);
+        return new NotaFiscal(fatura, valorImposto);
+    }
 }
