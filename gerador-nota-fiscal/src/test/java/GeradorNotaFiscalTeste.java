510e23a43baa1e0d227b3a3c4ca056cab3cf055b
luizzfe00 <luiz.felipe.farias@ccc.ufcg.edu.br>
2024-03-01 23:01:27 -0300

    teste calcula taxa para servico treinamento

--- gerador-nota-fiscal/src/test/java/GeradorNotaFiscalTeste.java
+++ gerador-nota-fiscal/src/test/java/GeradorNotaFiscalTeste.java
@@ -1,13 +1,24 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GeradorNotaFiscalTeste {
-        @Test
-        void calculaTaxaParaServicoConsultoria() {
-            Fatura fatura = new Fatura("Cliente1", "Endereco1", "CONSULTORIA", 1000.0);
-            NotaFiscalGenerator generator = new NotaFiscalGenerator();
+    @Test
+    void calculaTaxaParaServicoConsultoria() {
+        Fatura fatura = new Fatura("Cliente1", "Endereco1", "CONSULTORIA", 1000.0);
+        NotaFiscalGenerator generator = new NotaFiscalGenerator();
 
-            NotaFiscal notaFiscal = generator.gerarNotaFiscal(fatura);
+        NotaFiscal notaFiscal = generator.gerarNotaFiscal(fatura);
 
-            assertEquals(250.0, notaFiscal.getValorImposto(), 0.01);
-        }
+        assertEquals(250.0, notaFiscal.getValorImposto(), 0.01);
+    }
+
+    @Test
+    void calculaTaxaParaServicoTreinamento() {
+        Fatura fatura = new Fatura("Cliente2", "Endereco2", "TREINAMENTO", 800.0);
+        NotaFiscalGenerator generator = new NotaFiscalGenerator();
+
+        NotaFiscal notaFiscal = generator.gerarNotaFiscal(fatura);
+
+        assertEquals(120.0, notaFiscal.getValorImposto(), 0.01);
+    }
 }
