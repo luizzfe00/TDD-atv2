28108751466e3ec3d684e8c0200401afcd4fdcbf
luizzfe00 <luiz.felipe.farias@ccc.ufcg.edu.br>
2024-03-01 23:04:50 -0300

    teste envia email ao gerar nota fiscal

--- gerador-nota-fiscal/src/test/java/GeradorNotaFiscalTeste.java
+++ gerador-nota-fiscal/src/test/java/GeradorNotaFiscalTeste.java
@@ -1,13 +1,14 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GeradorNotaFiscalTeste {
     @Test
     void calculaTaxaParaServicoConsultoria() {
         Fatura fatura = new Fatura("Cliente1", "Endereco1", "CONSULTORIA", 1000.0);
-        NotaFiscalGenerator generator = new NotaFiscalGenerator();
+        GeradorNotaFiscal gerador = new GeradorNotaFiscal();
 
-        NotaFiscal notaFiscal = generator.gerarNotaFiscal(fatura);
+        NotaFiscal notaFiscal = gerador.gerarNotaFiscal(fatura);
 
         assertEquals(250.0, notaFiscal.getValorImposto(), 0.01);
     }
@@ -15,9 +16,9 @@ public class GeradorNotaFiscalTeste {
     @Test
     void calculaTaxaParaServicoTreinamento() {
         Fatura fatura = new Fatura("Cliente2", "Endereco2", "TREINAMENTO", 800.0);
-        NotaFiscalGenerator generator = new NotaFiscalGenerator();
+        GeradorNotaFiscal gerador = new GeradorNotaFiscal();
 
-        NotaFiscal notaFiscal = generator.gerarNotaFiscal(fatura);
+        NotaFiscal notaFiscal = gerador.gerarNotaFiscal(fatura);
 
         assertEquals(120.0, notaFiscal.getValorImposto(), 0.01);
     }
@@ -25,10 +26,20 @@ public class GeradorNotaFiscalTeste {
     @Test
     void calculaTaxaParaOutrosServicos() {
         Fatura fatura = new Fatura("Cliente3", "Endereco3", "OUTRO", 1500.0);
-        NotaFiscalGenerator generator = new NotaFiscalGenerator();
+        GeradorNotaFiscal gerador = new GeradorNotaFiscal();
 
-        NotaFiscal notaFiscal = generator.gerarNotaFiscal(fatura);
+        NotaFiscal notaFiscal = gerador.gerarNotaFiscal(fatura);
 
         assertEquals(90.0, notaFiscal.getValorImposto(), 0.01);
+    }
+
+    @Test
+    void enviaEmailAoGerarNotaFiscal() {
+        Fatura fatura = new Fatura("Cliente4", "Endereco4", "OUTRO", 2000.0);
+        GeradorNotaFiscal gerador = new GeradorNotaFiscal();
+
+        NotaFiscal notaFiscal = gerador.gerarNotaFiscal(fatura);
+
+        assertTrue(gerador.isEmailSent());
     }
 }
